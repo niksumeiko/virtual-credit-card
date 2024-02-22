f7501400cbd27afd37cb7bc280aed6177b39546c
Nik Sumeiko <nik.sumeiko@gmail.com>
2024-02-22 15:43:29 +0100

    feat: Provider

--- src/cards/__tests__/CreateVirtualCard.test.tsx
+++ src/cards/__tests__/CreateVirtualCard.test.tsx
@@ -1,10 +1,25 @@
-import { App } from '../../App';
+import { ContextProvider } from '../../common/context';
+import { provideVirtualCard } from '../useCases/createVirtualCard/VirtualCardProvider';
+import { VirtualCardPage } from '../useCases/createVirtualCard/VirtualCardPage';
 
 describe('Virtual card creation', () => {
     it('create and see virtual card', () => {
-        cy.mount(<App />, '/');
+        const fakeAdapter = () =>
+            Promise.resolve({
+                id: 1234567890111213,
+                holder: 'x',
+                expiry: new Date('2025-05-21T15:27:03.885Z'),
+                cvc: 123,
+                color: 'red',
+            });
 
-        cy.get('a[href*="/virtual-card"]').click();
+        cy.mount(
+            <ContextProvider providers={[provideVirtualCard(() => fakeAdapter)]}>
+                <VirtualCardPage />
+            </ContextProvider>,
+            '/',
+        );
+
         cy.get('[data-test="create-virtual-card"]').click();
 
         cy.get('[data-test="virtual-card"]').contains('1234').should('be.visible');
--- src/cards/useCases/createVirtualCard/useCreateVirtualCard.ts
+++ src/cards/useCases/createVirtualCard/useCreateVirtualCard.ts
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import type { CreditCard } from 'visa-client';
 import { createVirtualCardModel } from './VirtualCardPageService';
-import { createVirtualCardAdapter } from './VirtualCardAdapter';
+import { useVirtualCardAdapterFactory } from './VirtualCardProvider';
 
 export function useCreateVirtualCard() {
     const [card, setCard] = useState<CreditCard>();
-    const createVirtualCreditCard = createVirtualCardAdapter();
+    const factory = useVirtualCardAdapterFactory();
+    const createVirtualCreditCard = factory();
 
     const createCard = async () => {
         const candidate = await createVirtualCreditCard();
